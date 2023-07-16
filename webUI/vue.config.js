06063b225271d8496aba2773b1146d9e945d0264
Zhiyue Chen <zhiyuechen@Zhiyues-MacBook-Pro.local>
2023-07-16 17:00:37 +0800

    feat: add fonts

--- webUI/vue.config.js
+++ webUI/vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false
 })
+module.exports = {
+    pages: {
+      index: {
+        // entry for the pages
+        entry: 'src/views/index/index.js',
+        // the source template
+        template: 'src/views/index/index.html',
+        // output as dist/index.html
+        filename: 'index.html',
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: 'test',
+        // chunks to include on this pages, by default includes
+        // extracted common chunks and vendor chunks.
+        chunks: ['chunk-vendors', 'chunk-common', 'index']
+      }
+    }
+}
